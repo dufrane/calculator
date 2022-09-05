e35c6f1ba9fdf1d7a1b07a8bd32009bcb0deab0f
dufrane <dmitriy.vasilenko@gmail.com>
2022-09-05 22:14:07 +0200

    add logic

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -10,27 +10,38 @@ import Foundation
 
 struct CalculatorLogic {
     
-   
     private var number: Double?
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
     
     mutating func setNumber(_ number : Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         
         if let n = number {
             if symbol == "+/-" {
                 return n * -1
             } else if symbol == "AC" {
                 return 0
-            } else if symbol ==  "%"{
+            } else if symbol ==  "%" {
                 return n * 0.01
+            } else if symbol == "=" {
+                performTwoNumCalculation(n2: n)
+            } else {
+                intermediateCalculation = (n1: n, calcMethod: symbol)
             }
         }
         return nil
     }
     
+    private func performTwoNumCalculation(n2: Double) -> Double {
+        if let n1 = intermediateCalculation?.n1, let operation =  intermediateCalculation?.calcMethod {
+            if operation == "+" {
+                return n1 + n2
+            }
+        }
+    }
     
         
     
